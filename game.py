bce4e094fa0dca65c9e763ce7a2197803796ba46
NickWang <hcfw007@126.com>
2018-10-27 11:40:28 +0800

    add game class

---
+++ game.py
@@ -0,0 +1,12 @@
+from blocks import Blocks
+
+class Game:
+
+    score = 0
+    alive = True
+
+    def __init__(self, width = 10, height = 30):
+        self.width, self.height = width, height
+        self.gameBoard = [[0] * height] * width
+        self.borderSize = [width, height]
+        self.nextBlock = Blocks(self.borderSize)
